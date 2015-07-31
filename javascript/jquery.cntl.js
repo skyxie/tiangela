5c958e10754013e8ef2342d53fa9841ccfebf253
Tian Xie <tian@animoto.com>
2015-07-30 22:14:58 -0400

    Consolidate reveal code

--- javascript/jquery.cntl.js
+++ javascript/jquery.cntl.js
@@ -1,76 +1,64 @@
 (function($) {
 
-    $.fn.cntl = function( options ) {
+  $.fn.cntl = function(options) {
 
-        /* default settings */
-        var settings = $.extend({
-            revealbefore : 200, /* this is the amount of "scroll padding" to allow (the more, the later the state will be revealed) */
-            anim_class  : 'cntl-animate', /* the anim class, this class should have animation rules in css */
-            onreveal    : null /* a callback once the state has been revealed */
-        }, options);
+    /* default settings */
+    var settings = $.extend({
+      revealbefore : 200, /* this is the amount of "scroll padding" to allow (the more, the later the state will be revealed) */
+      anim_class  : 'cntl-animate', /* the anim class, this class should have animation rules in css */
+      statelist_class : 'cntl-state',
+      onreveal  : null, /* a callback once the state has been revealed */
+      bar_fill_class : null
+    }, options);
 
+    return this.each(function() {
+      var states = [];
+      var tbf = 0;
+      var statelist = $(this).find('.' + settings.statelist_class);
+      var bar_fill;
+      
+      if (settings.bar_fill_class != null) {
+        bar_fill = $(this).find('.' + settings.bar_fill_class);
+      }
 
+      function setupElements() {
+        for (var i = 0; i < statelist.length; i++) {
+          states[i] = {};
+          states[i]['top'] = $(statelist[i]).offset().top + settings.revealbefore;
+          states[i]['elm'] = $(statelist[i]);
+        };
 
+        revealElements();
+      }
 
-        return this.each( function() {
+      function revealElements() {
+        var windowtop = $(window).scrollTop();
+        var windowbtm = windowtop + $(window).height();
 
-            var statelist = $(this).find('.cntl-state');
-            var bar_fill = $(this).find('.cntl-bar-fill');
-            var states = [];
-            var tbf = 0;
-
-
-            function setupElements( )
-            {
-
-                for (var i = 0; i < statelist.length; i++) {
-
-                    states[i] = {};
-                    states[i]['top'] = $(statelist[i]).offset().top + settings.revealbefore;
-                    states[i]['elm'] = $(statelist[i]);
-
-                };
-
-
-                revealElements();
-
+        for (var i = 0; i < states.length; i++) {
+          if (states[i].top > windowtop && states[i].top < windowbtm) {
+            if (!states[i].elm.hasClass(settings.anim_class) && $.isFunction(settings.onreveal)) {
+              settings.onreveal.call(this, states[i].elm);
             }
 
-            function revealElements( )
-            {
+            states[i].elm.addClass(settings.anim_class);
+            var h = states[i].elm.position().top;
 
-                var windowtop = $(window).scrollTop();
-                var windowbtm = windowtop + $(window).height();
-
-                for( var i = 0; i < states.length; i++) {
-
-                    if( states[i].top > windowtop && states[i].top < windowbtm )
-                    {
-                        if ( 
-                            !states[i].elm.hasClass( settings.anim_class ) && 
-                            $.isFunction( settings.onreveal ) )
-                        {
-                            settings.onreveal.call( this, states[i].elm );
-                        }
-
-                        states[i].elm.addClass( settings.anim_class );
-                        var h = states[i].elm.position().top;
-
-                        if( h > tbf )
-                        {
-                            tbf = h;
-                        }
-                        bar_fill.height( tbf );
-
-                    }
-                };
-
+            if (h > tbf) {
+              tbf = h;
             }
 
-            $(window).on('scroll',revealElements);
-            $(window).on('load',setupElements)
+            if (bar_fill != null) {
+              bar_fill.height(tbf);
+            }
+          }
+        };
+      }
 
-        });
-    }
+      $(window).on('scroll',revealElements);
+      $(window).on('load',setupElements)
+
+    });
+  }
 
 }(jQuery));
